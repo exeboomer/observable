27be162de3624eb4f2e09f1ad7a699f76b37fc4a
exeboomer <otknet@yandex.ru>
2018-03-04 03:30:22 -0800

    trytrytry

--- observable.js
+++ observable.js
@@ -1,38 +1,47 @@
-// Observable - лифт
-
-function evalator(){ //лифт
-  var humans = []; // список наблюдающих
-  this.sendMessage = function(msg){ //Оповещение всех наблюдающих
-    for (var i = 0, len = humans.length; i < len; i++){
-      humans[i].imhere(msg); // для оповещения обращается к методу imhere
+class Bus {
+    constructor () {
+        // Ключ - имя комнаты, значение - массив подписчиков
+        this.rooms = {}
     }
-  };
-  this.addHuman = function(human){ //Добавление человека к списку наблюдающих
-    humans.push(human);
-  };
+    /**
+     * @param {String} room - Имя комнаты
+     * @param {Object} message - Сообщение
+     * @return {undefined}
+     */
+    publish (room, message) {
+        if(room in this.rooms){
+            this.rooms[room].forEach(function(subscriber) {
+                subscriber(message);
+            })
+        }
+    }
+
+    /**
+     * @param {String} room - Имя комнаты
+     * @param {Function} handler - Обработчик сообщения
+     * @return {undefined}
+     */
+    subscribe (room, handler) {
+        if(!Array.isArray(this.rooms[handler])){
+            this.rooms[handler] = [];
+        }
+        if(room in this.rooms){
+            this.rooms[handler].push(room);
+        }
+    }
+
 }
 
-//[observers] - люди
+// Не редактируйте всё, что ниже
 
-function human(behavior){ // передаем поведение
-  this.imhere = function(msg){ 
-    behavior(msg); // порядок обработки полученного сообщения
-  };
-}
+const bus = new Bus();
 
-var evalator = new evalator(); //создал лифт
+bus.subscribe('chat', message => console.log(`[${message.sender}]: ${message.text}`))
 
-var hum1 = new human(function(msg){console.log("oh evalator, but now "+msg);}); // создал первого человека + его поведение
-var hum2 = new human(function(msg){console.log("evalator here at "+msg);}); // создал первого человека + его поведение
+bus.subscribe('chat', message => {
+    if (message.sender !== 'Echo') {
+        bus.publish('chat', { sender: 'Echo', text: message.text })
+    }
+})
 
-evalator.addHuman(hum1); //добавляем в список человека1
-evalator.addHuman(hum2); //добавляем в список человека2
-
-setTimeout(function(){evalator.sendMessage('time ' + new Date());}, 4000) // поведение лифта
-
-/*
-Есть лифт, за ним наблюдают 2 человека.
-Лифт приезжает и оповещает их о прибытии передавая время.
-Один кричит ох, лифт здесь, но сейчас столько-то времени.
-Второй говорит лифт здесь в столько-то времени...
-*/
+bus.publish('chat', { sender: 'Oz', text: 'Hello' })
